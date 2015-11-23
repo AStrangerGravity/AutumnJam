9e07f2236436e53b56e6681e99d4e8b35cdab779
Matt Blair <blair1618@gmail.com>
2015-11-23 00:22:06 -0500

    Add Tree.cs for managing an endlessly-traversable tree

---
+++ Assets/Tree.cs
@@ -0,0 +1,267 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tree : MonoBehaviour {
+
+    public GameObject child_node_prefab;
+    public GameObject parent_node_prefab;
+
+    public struct NodeData {
+        public int type;
+        public int child_index;
+        public int parent_index;
+
+        public NodeData(int type, int child_index, int parent_index) {
+            this.type = type;
+            this.child_index = child_index;
+            this.parent_index = parent_index;
+        }
+    }
+
+    public Color ColorForNodeType(int type) {
+        
+        switch (type) {
+            case 0: return Color.red;
+            case 1: return Color.yellow;
+            case 2: return Color.green;
+            case 3: return Color.magenta;
+        }
+        return Color.black;
+        
+    }
+
+    public const int n_children = 3;
+    public const int n_types = 4;
+
+    protected List<NodeData> nodes = new List<NodeData>();
+    protected int current_node = 0;
+    protected MeshRenderer[] visible_meshes = new MeshRenderer[n_children];
+    protected ClickListener[] child_listeners = new ClickListener[n_children];
+    protected ClickListener parent_listener;
+
+    // Use this for initialization
+    void Start() {
+
+        // Add n_children initial nodes
+        CreateSiblingGroup(current_node);
+
+        // Initialize meshes
+        float depth = 7;
+        for (int i = 0; i < n_children; ++i) {
+
+            float angle = 2 * Mathf.PI * i / (float) n_children;
+            float radius = 3;
+            Vector3 position = new Vector3(radius * Mathf.Sin(angle), radius * Mathf.Cos(angle), depth);
+
+            GameObject child_node_instance = (GameObject) Instantiate(child_node_prefab, position, Quaternion.identity);
+            visible_meshes[i] = child_node_instance.GetComponent<MeshRenderer>();
+            child_listeners[i] = child_node_instance.GetComponent<ClickListener>();
+
+        }
+
+        GameObject parent_node_instance = (GameObject) Instantiate(parent_node_prefab, new Vector3(0, 0, depth), Quaternion.identity);
+        parent_listener = parent_node_instance.GetComponent<ClickListener>();
+
+        UpdateMeshes(current_node);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // If a visible node is selected, descend into its sub-tree
+        for (int i = 0; i < n_children; ++i) {
+
+            if (child_listeners[i].Clicked()) {
+                Descend(i);
+                break;
+            }
+
+        }
+
+        // If the parent node is selected, ascend to its super-tree
+        if (parent_listener.Clicked()) {
+            Ascend();
+        }
+
+        RotateMeshes(Time.deltaTime);
+
+    }
+
+    void Ascend() {
+
+        // Move up to the super-tree of the current visible nodes
+        int next_node = nodes[current_node].parent_index;
+
+        if (next_node < 0) {
+
+            // Create new nodes at the super-tree
+            next_node = nodes.Count;
+            CreateSiblingGroup(next_node);
+
+            // Choose a random position for the parent among its siblings
+            next_node += Random.Range(0, n_children);
+
+            // Connect parent to children
+            for (int i = 0; i < n_children; ++i) {
+                SetParentIndex(current_node + i, next_node);
+            }
+            SetChildIndex(next_node, current_node);
+
+        }
+
+        // Ensure new current node is consistent with child types
+        SetType(next_node, ParentNodeType(current_node));
+
+        // Set current node to the first child in the current group of nodes
+        current_node = next_node - (next_node % n_children);
+
+        // Update meshes to match newly visible nodes
+        UpdateMeshes(current_node);
+
+    }
+
+    void Descend(int child) {
+
+        // Move down to the sub-tree of the chosen child
+        int next_node = nodes[current_node + child].child_index;
+
+        if (next_node < 0) {
+
+            // Create new nodes at the sub-tree
+            int current_type = nodes[current_node + child].type;
+            next_node = nodes.Count;
+
+            do {
+
+                CreateSiblingGroup(next_node);
+
+            } while (ParentNodeType(next_node) != current_type);
+
+            // Connect parent to children
+            for (int i = 0; i < n_children; ++i) {
+                SetParentIndex(next_node + i, current_node + child);
+            }
+            SetChildIndex(current_node + child, next_node);
+
+        }
+
+        current_node = next_node;
+
+        // Update meshes to match newly visible nodes
+        UpdateMeshes(current_node);
+
+    }
+
+    void CreateSiblingGroup(int start_index, int parent_index = -1) {
+
+        // Ensure the node list has enough members to initialize n_children nodes
+        // starting at the given position
+        int nodes_to_add = start_index + n_children - nodes.Count;
+
+        while (nodes_to_add-- > 0) {
+
+            nodes.Add(new NodeData(-1, -1, -1));
+
+        }
+
+        // Initialize n_children nodes starting at the given position
+        for (int i = 0; i < n_children; ++i) {
+
+            nodes[start_index + i] = new NodeData(Random.Range(0, n_types), -1, parent_index);
+
+        }
+
+    }
+
+    void SetParentIndex(int index, int parent_index) {
+
+        NodeData node = nodes[index];
+        node.parent_index = parent_index;
+        nodes[index] = node;
+
+    }
+
+    void SetChildIndex(int index, int child_index) {
+
+        NodeData node = nodes[index];
+        node.child_index = child_index;
+        nodes[index] = node;
+
+    }
+
+    void SetType(int index, int type) {
+
+        NodeData node = nodes[index];
+        node.type = type;
+        nodes[index] = node;
+
+    }
+
+    int ParentNodeType(int start_index) {
+
+        // Count the number of nodes of each type
+        int[] counts = new int[n_types];
+        for (int i = 0; i < n_children; ++i) {
+
+            int type = nodes[start_index + i].type;
+            ++counts[type];
+
+        }
+
+        // Find the types with the highest and lowest counts
+        int highest_type = 0;
+        int absent_type = 0;
+        int max_count = 0;
+        int n_counts_one = 0;
+        for (int i = 0; i < n_types; ++i) {
+
+            int c = counts[i];
+
+            if (c > max_count) {
+                max_count = c;
+                highest_type = i;
+            }
+            if (c == 0) {
+                absent_type = i;
+            }
+            if (c == 1) {
+                ++n_counts_one;
+            }
+
+        }
+
+        // If all types but one have a count of one, result is the left-out type
+        if (n_counts_one == n_types - 1) {
+            return absent_type;
+        }
+
+        return highest_type;
+
+    }
+
+    void UpdateMeshes(int index) {
+
+        for (int i = 0; i < visible_meshes.Length; ++i) {
+
+            Color color = ColorForNodeType(nodes[index + i].type);
+
+            MeshRenderer mesh = visible_meshes[i];
+            mesh.material.color = color;
+            mesh.transform.rotation = Quaternion.identity;
+
+        }
+
+    }
+
+    void RotateMeshes(float delta_time) {
+
+        float delta_angle = 10 * delta_time;
+        foreach (var mesh in visible_meshes) {
+            mesh.transform.Rotate(delta_angle, delta_angle, 0);
+        }
+
+    }
+
+}
