ddded290ddf725dac0b18e98043a56bf246ed231
Matt Blair <blair1618@gmail.com>
2015-11-23 00:22:46 -0500

    Add ClickListener.cs to listen for clicks on colliders

---
+++ Assets/ClickListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickListener : MonoBehaviour {
+
+	protected bool clicked = false;
+
+	void OnMouseDown() {
+
+		clicked = true;
+
+	}
+
+	public bool Clicked(bool reset = true) {
+
+		bool c = clicked;
+
+		if (reset) {
+			clicked = false;
+		}
+
+		return c;
+
+	}
+}
