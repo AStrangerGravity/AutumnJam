a3b74e0bcc679f9725529ff837854416cbe683fe
John Austin <john@astrangergravity.com>
2015-11-29 23:08:17 -0800

    I did two things (regrettably without seprate commits).  - Reformated using Resharper (looking at adding this to our build process).  - Changed the display from circular to grid and tweaked some of the properties (only 8 children now)

--- Assets/Tree.cs
+++ Assets/Tree.cs
@@ -1,23 +1,25 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Tree : MonoBehaviour {
+    // (0...1): Chance of next type in generated layer following the previous generated type.
+    private readonly float HOMOGENITY = .8f;
+
+    public const int n_children = 8; 
+    public const int n_types = 10;
+    protected ClickListener[] child_listeners = new ClickListener[n_children];
 
     public GameObject child_node_prefab;
+    protected GameObject[] child_nodes = new GameObject[n_children];
+    protected int current_node;
+
+    protected List<NodeData> nodes = new List<NodeData>();
+    protected ClickListener parent_listener;
+
+    protected GameObject parent_node_instance;
     public GameObject parent_node_prefab;
-
-    public struct NodeData {
-        public int type;
-        public int child_index;
-        public int parent_index;
-
-        public NodeData(int type, int child_index, int parent_index) {
-            this.type = type;
-            this.child_index = child_index;
-            this.parent_index = parent_index;
-        }
-    }
+    protected MeshRenderer[] visible_meshes = new MeshRenderer[n_children];
 
     public Color ColorForNodeType(int type) {
         switch (type) {
@@ -31,43 +33,48 @@ public class Tree : MonoBehaviour {
             case 7: return Color.grey;
             case 8: return Color.black;
             case 9: return Color.clear;
-            default: return Color.black;
+            default:
+                return Color.black;
         }
     }
 
-    public const int n_children = 20;
-    public const int n_types = 5;
-
-    protected List<NodeData> nodes = new List<NodeData>();
-    protected int current_node = 0;
-    protected MeshRenderer[] visible_meshes = new MeshRenderer[n_children];
-    protected ClickListener[] child_listeners = new ClickListener[n_children];
-    protected GameObject[] child_nodes = new GameObject[n_children];
-    protected ClickListener parent_listener;
-
-    protected GameObject parent_node_instance;
-
     // Use this for initialization
-    void Start() {
+    private void Start() {
         // Add n_children initial nodes
         CreateSiblingGroup(current_node, null);
 
         // Initialize meshes
-        float depth = 7;
-        for (int i = 0; i < n_children; ++i) {
+        const float depth = 7;
 
-            float angle = 2 * Mathf.PI * i / (float) n_children;
-            float radius = 3f;
-            Vector3 position = new Vector3(radius * Mathf.Sin(angle), radius * Mathf.Cos(angle), depth);
+        // +1 because we don't place a node at the center
+        int child_index = 0;
+        for (int i = 0; i < n_children + 1; ++i) {
+            //float angle = 2 * Mathf.PI * i / (float) n_children;
+            //float radius = 3f;
+            //Vector3 position = new Vector3(radius * Mathf.Sin(angle), radius * Mathf.Cos(angle), depth);
+            const float spacing = 1.5f;
+            int row_size = Mathf.CeilToInt(Mathf.Sqrt(n_children));
+            float offset = -(spacing * (row_size-1)) / 2f;
+            int x = i % row_size;
+            int y = i / row_size;
 
-            GameObject child_node_instance = (GameObject) Instantiate(child_node_prefab, position, Quaternion.identity);
-            visible_meshes[i] = child_node_instance.GetComponent<MeshRenderer>();
-            child_listeners[i] = child_node_instance.GetComponent<ClickListener>();
-            child_nodes[i] = child_node_instance;
+            if (x == row_size/2 && y == row_size/2) {
+                continue;
+            }
 
+
+            Vector3 position = new Vector3(offset + spacing * x, offset + spacing * y, depth);
+
+            GameObject child_node_instance =
+                (GameObject) Instantiate(child_node_prefab, position, Quaternion.identity);
+            visible_meshes[child_index] = child_node_instance.GetComponent<MeshRenderer>();
+            child_listeners[child_index] = child_node_instance.GetComponent<ClickListener>();
+            child_nodes[child_index] = child_node_instance;
+            child_index++;
         }
 
-        parent_node_instance = (GameObject) Instantiate(parent_node_prefab, new Vector3(0, 0, depth), Quaternion.identity);
+        parent_node_instance = (GameObject)
+            Instantiate(parent_node_prefab, new Vector3(0, 0, depth), Quaternion.identity);
         parent_listener = parent_node_instance.GetComponent<ClickListener>();
 
         UpdateMeshes(current_node);
@@ -76,16 +83,13 @@ public class Tree : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
-
+    private void Update() {
         // If a visible node is selected, descend into its sub-tree
         for (int i = 0; i < n_children; ++i) {
-
             if (child_listeners[i].Clicked()) {
                 Descend(i);
                 break;
             }
-
         }
 
         // If the parent node is selected, ascend to its super-tree
@@ -94,15 +98,14 @@ public class Tree : MonoBehaviour {
         }
 
         //RotateMeshes(Time.deltaTime);
-
     }
 
-    void Ascend() {
-    iTween.MoveTo(Camera.main.gameObject, new Hashtable(){
+    private void Ascend() {
+        iTween.MoveTo(Camera.main.gameObject, new Hashtable {
             {"position", parent_node_instance.transform.position - new Vector3(0, 0, 20)},
             {"time", 1f},
             {"oncomplete", "AfterZoomToParent"},
-            {"oncompletetarget", gameObject }
+            {"oncompletetarget", gameObject}
         });
 
         iTween.CameraFadeTo(1, 1);
@@ -113,7 +116,6 @@ public class Tree : MonoBehaviour {
         int next_node = nodes[current_node].parent_index;
 
         if (next_node < 0) {
-
             // Create new nodes at the super-tree
             next_node = nodes.Count;
             CreateSiblingGroup(next_node, null);
@@ -126,14 +128,13 @@ public class Tree : MonoBehaviour {
                 SetParentIndex(current_node + i, next_node);
             }
             SetChildIndex(next_node, current_node);
-
         }
 
         // Ensure new current node is consistent with child types
         SetType(next_node, ParentNodeType(current_node));
 
         // Set current node to the first child in the current group of nodes
-        current_node = next_node - (next_node % n_children);
+        current_node = next_node - next_node % n_children;
 
         // Update meshes to match newly visible nodes
         UpdateMeshes(current_node);
@@ -141,18 +142,18 @@ public class Tree : MonoBehaviour {
         Camera.main.transform.position = parent_node_instance.transform.position;
         iTween.CameraFadeTo(0, .25f);
 
-        iTween.MoveTo(Camera.main.gameObject, new Hashtable(){
-            {"position", parent_node_instance.transform.position - new Vector3(0, 0, 8)},
+        iTween.MoveTo(Camera.main.gameObject, new Hashtable {
+            {"position", parent_node_instance.transform.position - new Vector3(0, 0, 8)}
         });
     }
 
-    void Descend(int child) {
-        iTween.MoveTo(Camera.main.gameObject, new Hashtable(){
+    private void Descend(int child) {
+        iTween.MoveTo(Camera.main.gameObject, new Hashtable {
             {"position", child_nodes[child].transform.position + new Vector3(0, 0, 1)},
             {"time", 1f},
             {"oncomplete", "AfterZoomToChild"},
-            {"oncompleteparams", child },
-            {"oncompletetarget", gameObject }
+            {"oncompleteparams", child},
+            {"oncompletetarget", gameObject}
         });
 
         iTween.CameraFadeTo(1, 1);
@@ -163,15 +164,12 @@ public class Tree : MonoBehaviour {
         int next_node = nodes[current_node + child].child_index;
 
         if (next_node < 0) {
-
             // Create new nodes at the sub-tree
             int current_type = nodes[current_node + child].type;
             next_node = nodes.Count;
 
             do {
-
                 CreateSiblingGroup(next_node, current_type);
-
             } while (ParentNodeType(next_node) != current_type);
 
             // Connect parent to children
@@ -179,7 +177,6 @@ public class Tree : MonoBehaviour {
                 SetParentIndex(next_node + i, current_node + child);
             }
             SetChildIndex(current_node + child, next_node);
-
         }
 
         current_node = next_node;
@@ -189,26 +186,25 @@ public class Tree : MonoBehaviour {
 
         // Add a slight shift to make it feel like we're coming from the direction
         // of the node we descended to
-        Vector3 shift = child_nodes[child].transform.position - parent_node_instance.transform.position;
+        Vector3 shift = child_nodes[child].transform.position -
+                        parent_node_instance.transform.position;
 
-        Camera.main.transform.position = parent_node_instance.transform.position - new Vector3(0, 0, 100) - shift * 10;
+        Camera.main.transform.position = parent_node_instance.transform.position -
+                                         new Vector3(0, 0, 100) - shift * 10;
         iTween.CameraFadeTo(0, .25f);
 
-        iTween.MoveTo(Camera.main.gameObject, new Hashtable(){
-            {"position", parent_node_instance.transform.position - new Vector3(0, 0, 8)},
+        iTween.MoveTo(Camera.main.gameObject, new Hashtable {
+            {"position", parent_node_instance.transform.position - new Vector3(0, 0, 8)}
         });
     }
 
-    void CreateSiblingGroup(int start_index, int? ideal_type, int parent_index = -1) {
-
+    private void CreateSiblingGroup(int start_index, int? ideal_type, int parent_index = -1) {
         // Ensure the node list has enough members to initialize n_children nodes
         // starting at the given position
         int nodes_to_add = start_index + n_children - nodes.Count;
 
         while (nodes_to_add-- > 0) {
-
             nodes.Add(new NodeData(-1, -1, -1));
-
         }
 
         // Initialize n_children nodes starting at the given position
@@ -217,51 +213,40 @@ public class Tree : MonoBehaviour {
             int t = Random.Range(0, n_types);
 
             // 50% change of using the type of the previous node
-            if (Random.value > .5f) {
+            if (Random.value < HOMOGENITY) {
                 t = past_type;
             }
 
             nodes[start_index + i] = new NodeData(t, -1, parent_index);
 
             past_type = t;
-
         }
-
     }
 
-    void SetParentIndex(int index, int parent_index) {
-
+    private void SetParentIndex(int index, int parent_index) {
         NodeData node = nodes[index];
         node.parent_index = parent_index;
         nodes[index] = node;
-
     }
 
-    void SetChildIndex(int index, int child_index) {
-
+    private void SetChildIndex(int index, int child_index) {
         NodeData node = nodes[index];
         node.child_index = child_index;
         nodes[index] = node;
-
     }
 
-    void SetType(int index, int type) {
-
+    private void SetType(int index, int type) {
         NodeData node = nodes[index];
         node.type = type;
         nodes[index] = node;
-
     }
 
-    int ParentNodeType(int start_index) {
-
+    private int ParentNodeType(int start_index) {
         // Count the number of nodes of each type
-        int[] counts = new int[n_types];
+        var counts = new int[n_types];
         for (int i = 0; i < n_children; ++i) {
-
             int type = nodes[start_index + i].type;
             ++counts[type];
-
         }
 
         // Find the types with the highest and lowest counts
@@ -270,7 +255,6 @@ public class Tree : MonoBehaviour {
         int max_count = 0;
         int n_counts_one = 0;
         for (int i = 0; i < n_types; ++i) {
-
             int c = counts[i];
 
             if (c > max_count) {
@@ -283,7 +267,6 @@ public class Tree : MonoBehaviour {
             if (c == 1) {
                 ++n_counts_one;
             }
-
         }
 
         // If all types but one have a count of one, result is the left-out type
@@ -292,30 +275,34 @@ public class Tree : MonoBehaviour {
         }
 
         return highest_type;
-
     }
 
-    void UpdateMeshes(int index) {
-
+    private void UpdateMeshes(int index) {
         for (int i = 0; i < visible_meshes.Length; ++i) {
-
             Color color = ColorForNodeType(nodes[index + i].type);
 
             MeshRenderer mesh = visible_meshes[i];
             mesh.material.color = color;
             mesh.transform.rotation = Quaternion.identity;
-
         }
-
     }
 
-    void RotateMeshes(float delta_time) {
-
+    private void RotateMeshes(float delta_time) {
         float delta_angle = 10 * delta_time;
-        foreach (var mesh in visible_meshes) {
+        foreach (MeshRenderer mesh in visible_meshes) {
             mesh.transform.Rotate(delta_angle, delta_angle, 0);
         }
-
     }
 
+    public struct NodeData {
+        public int type;
+        public int child_index;
+        public int parent_index;
+
+        public NodeData(int type, int child_index, int parent_index) {
+            this.type = type;
+            this.child_index = child_index;
+            this.parent_index = parent_index;
+        }
+    }
 }
