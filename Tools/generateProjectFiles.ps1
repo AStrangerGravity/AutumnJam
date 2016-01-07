356f9e3e8169a2d740aadaea8ffaf70705d92976
John Austin <john@astrangergravity.com>
2016-01-07 00:12:32 -0800

    Script to generate vs files.

---
+++ Tools/generateProjectFiles.ps1
@@ -0,0 +1,14 @@
+# Windows Only
+# Generates the Visual Studio solution files so that we can perform linting and other CI analysis on it.
+
+# Print each command as we go
+# Set-PSDebug -Step
+
+# Find our project path
+$tools_dir = Split-Path $MyInvocation.MyCommand.Path
+$project_dir = Join-Path $tools_dir "/../" -resolve
+
+# Query the assembly function to generate the project
+$exitCode = [Diagnostics.Process]::Start("C:\Program Files\Unity\Editor\Unity.exe", 
+    "-batchmode -logFile -projectPath $project_dir -executeMethod SyntaxTree.VisualStudio.Unity.Bridge.ProjectFilesGenerator.GenerateProject -quit").WaitForExit(60000) # Wait for 60 seconds
+
