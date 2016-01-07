4bb5c7661a3ce3fc86f5f19bf7f046d26ca4f811
John Austin <john@astrangergravity.com>
2016-01-07 00:58:37 -0800

    Try adding error catching code.

--- AutumnJam/Tools/generateProjectFiles.ps1
+++ AutumnJam/Tools/generateProjectFiles.ps1
@@ -3,7 +3,7 @@
 
 # Print each command as we go
 # Set-PSDebug -Step
-
+try {
 # Find our project path
 $tools_dir = Split-Path $MyInvocation.MyCommand.Path
 $project_dir = Join-Path $tools_dir "/../" -resolve
@@ -12,3 +12,10 @@ $project_dir = Join-Path $tools_dir "/../" -resolve
 $exitCode = [Diagnostics.Process]::Start("C:\Program Files\Unity\Editor\Unity.exe", 
     "-batchmode -logFile -projectPath $project_dir -executeMethod SyntaxTree.VisualStudio.Unity.Bridge.ProjectFilesGenerator.GenerateProject -quit").WaitForExit(60000) # Wait for 60 seconds
 
+    
+  # your code here
+} Catch {
+  $ErrorMessage = $_.Exception.Message
+  Write-Output $ErrorMessage
+  exit(1)
+}
