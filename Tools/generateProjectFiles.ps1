dde010a5ed027de3c22faa8993dfaf65d0e565f4
John Austin <john@astrangergravity.com>
2016-01-07 00:19:32 -0800

    Refix.

--- Tools/generateProjectFiles.ps1
+++ Tools/generateProjectFiles.ps1
@@ -5,10 +5,10 @@
 # Set-PSDebug -Step
 
 # Find our project path
-$working_dir = Split-Path $MyInvocation.MyCommand.Path
-#$project_dir = Join-Path $working_dir "/../" -resolve
+$tools_dir = Split-Path $MyInvocation.MyCommand.Path
+$project_dir = Join-Path $tools_dir "/../" -resolve
 
 # Query the assembly function to generate the project
 $exitCode = [Diagnostics.Process]::Start("C:\Program Files\Unity\Editor\Unity.exe", 
-    "-batchmode -logFile -projectPath $working_dir -executeMethod SyntaxTree.VisualStudio.Unity.Bridge.ProjectFilesGenerator.GenerateProject -quit").WaitForExit(60000) # Wait for 60 seconds
+    "-batchmode -logFile -projectPath $tools_dir -executeMethod SyntaxTree.VisualStudio.Unity.Bridge.ProjectFilesGenerator.GenerateProject -quit").WaitForExit(60000) # Wait for 60 seconds
 
